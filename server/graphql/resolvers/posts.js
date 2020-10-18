5de5a73eaa22e6f507112a6d3afeba68f27deb09
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-10-18 20:18:35 -0300

    user can now create posts

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -30,7 +30,7 @@ module.exports = {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
-      if (args.body.trim() === "") {
+      if (body.trim() === "") {
         throw new Error("Post body must not be empty");
       }
 
@@ -49,7 +49,6 @@ module.exports = {
 
       return post;
     },
-
     async deletePost(_, { postId }, context) {
       const user = checkAuth(context);
 
@@ -57,7 +56,7 @@ module.exports = {
         const post = await Post.findById(postId);
         if (user.username === post.username) {
           await post.delete();
-          return "Post deleted succesfully";
+          return "Post deleted successfully";
         } else {
           throw new AuthenticationError("Action not allowed");
         }
@@ -71,9 +70,8 @@ module.exports = {
       const post = await Post.findById(postId);
       if (post) {
         if (post.likes.find((like) => like.username === username)) {
-          // Post already likes, unlike
+          // Post already likes, unlike it
           post.likes = post.likes.filter((like) => like.username !== username);
-          await post.save();
         } else {
           // Not liked, like post
           post.likes.push({
@@ -87,7 +85,6 @@ module.exports = {
       } else throw new UserInputError("Post not found");
     },
   },
-
   Subscription: {
     newPost: {
       subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
