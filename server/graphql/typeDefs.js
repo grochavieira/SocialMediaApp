eaceca85b3c9f19860401c9b7f156a8560e4bf67
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-10-18 12:27:17 -0300

    implemented post like and comments

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -1,11 +1,28 @@
 const { gql } = require("apollo-server");
 
 module.exports = gql`
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
+  }
+
   type Post {
     id: ID!
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
   }
 
   type User {
@@ -33,5 +50,12 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
+  }
+
+  type Subscription {
+    newPost: Post!
   }
 `;
